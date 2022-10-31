d5d3ecde05df44d816cefa6f522097147f7f58ee
Drew Payton <drewpayton18@gmail.com>
2022-10-30 20:20:18 -0500

    addint the cell placements and updated it in a nested forloop

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
+from ctypes import util
 from tkinter import *
+from cell import Cell
+import settings
+import utils
 
 root = Tk()
+# Override the settings of the window
 root.configure(bg="black")
-root.geometry('1440x720')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper")
 root.resizable(False, False)
+
+
+top_frame = Frame(
+    root,
+    bg='black',  # Change later to black,
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
+
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg="black",  # Change later to black
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg="black",  # Change later to black
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25)
+)
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x,
+            row=y
+        )
+
+# Run the window
 root.mainloop()
